ee7e242d7ac6770462128d5c9aefff3c7e817838
alexpts <alexpts@yandex.ru>
2018-01-05 18:23:53 +0300

    init

---
+++ Readme.md
@@ -0,0 +1,50 @@
+winston-transport-sentry
+========================
+
+
+### Compitable
+Winston@3+
+
+Sentry transport for winston@3 logger for node js
+-------------------------------------------------
+Follow this sample configuration to use:
+
+```javascript
+const winston = require('winston'),
+const Mail = require('winston-mail').Mail,
+const Sentry = require('winston-transport-sentry');
+
+var logger = new winston.Logger({
+    transports: [
+        new winston.transports.Console({level: 'silly'}),
+        new Sentry({
+            level: 'warn',
+            dsn: "{{ YOUR SENTRY DSN }}",
+            tags: { key: 'value' },
+            extra: { key: 'value' },
+            patchGlobal: false
+        })
+    ],
+});
+```
+
+If you want to use patchGlobal to catch all uncaught errors, simply pass it as option like this:
+
+```javascript
+new Sentry({
+    patchGlobal: true
+});
+```
+
+Winston logging levels are mapped to the default sentry levels like this:
+
+```javascript
+{
+    silly: 'debug',
+    verbose: 'debug',
+    info: 'info',
+    debug: 'debug',
+    warn: 'warning',
+    error: 'error'
+}
+```
---
+++ index.js
@@ -0,0 +1,101 @@
+const Raven = require('raven');
+const Transport = require('winston-transport');
+const _ = require('lodash');
+const util = require('util');
+
+const winstonLevelToSentryLevel = {
+    silly: 'debug',
+    verbose: 'debug',
+    info: 'info',
+    debug: 'debug',
+    warn: 'warning',
+    error: 'error'
+};
+
+/**
+ * @param {Error} error
+ */
+const errorHandler = (error) => {
+    console.log(error);
+};
+
+/**
+ * @param {{}} info
+ * @param {string} info.level
+ * @return {{}}
+ */
+const prepareMeta = (info) => {
+    let extra = Object.assign({}, info);
+    delete extra.message;
+    delete extra.level;
+    delete extra.tags;
+
+    let error = info.message instanceof Error ? info.message : new Error(info.message);
+    extra.stackError = error.stack;
+
+    return {
+        level: winstonLevelToSentryLevel[info.level],
+        tags: info.tags || {},
+        extra,
+    };
+};
+
+/**
+ * @param {{}} options
+ * @return {{}}
+ */
+const createRaven = (options) => {
+    const raven = Raven.config(options.dsn, options);
+    raven.captureException = util.promisify(raven.captureException);
+    raven.captureMessage = util.promisify(raven.captureMessage);
+
+    if (options.install || options.patchGlobal) {
+        raven.install();
+    }
+
+    if (options.errorHandler && _.isFunction(options.errorHandler)) {
+        raven.on('error', options.errorHandler);
+    }
+
+    return raven;
+};
+
+class SentryWinstonTransport extends Transport {
+    constructor(options) {
+        super(options);
+
+        this.options = Object.assign({
+            dsn: '',
+            patchGlobal: false,
+            install: false,
+            tags: {},
+            extra: {},
+            errorHandler,
+        }, options);
+
+        this.raven = createRaven(this.options);
+    }
+
+    /**
+     * @param {{}} info
+     * @param {string} info.level
+     * @param {Error|string} info.message
+     * @param {Function} done
+     */
+    async log(info, done) {
+        if (this.silent) return done(null, true);
+        let meta = prepareMeta(info);
+
+        let method = info.message === 'error' ? 'captureException' : 'captureMessage';
+
+        try {
+            let eventId = await this.raven[method](info.message, meta);
+            done(null, eventId);
+        } catch (error) {
+            done(error);
+        }
+    }
+}
+
+SentryWinstonTransport.prototype.name = 'sentry';
+module.exports = SentryWinstonTransport;
